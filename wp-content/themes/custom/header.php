5a779b7531f425cc389d871075d263b5b1853946
Phuc Le <le.phuc.h@gmail.com>
2017-01-02 13:33:56 -0800

    Remove empty script from header.

--- wp-content/themes/custom/header.php
+++ wp-content/themes/custom/header.php
@@ -15,9 +15,7 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/include/session.inc.php');
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <script src="/script/bootstrap.min.js"></script>
 <script src="/js/mobile_menu.js"></script>
-<script type="text/javascript">
 
-</script>
 <?php if ( !is_user_logged_in() ){ ?>
 	    <style>
             #wpadminbar{ display:none; }
