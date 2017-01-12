e16a647363082a1963507489bc6287e9851206fd
Phuc Le <le.phuc.h@gmail.com>
2017-01-11 17:06:39 -0800

    Add export functionality to comms tracking.

--- tracking/comms.php
+++ tracking/comms.php
@@ -30,30 +30,32 @@ $everyone = getCommTracking();
 <h3>Comm & Due Credit</h3>
 <form action="/tracking/input.php" method="POST">
 <?php forms_hiddenInput('setcommtracking', '/tracking/comms.php') ?>
-<table id="name-list" class="show-table">
-	<tr>
-		<th>Name</th>
-		<th>Credit?</th>
-		<th>Dues?</th>
-		<th>Details</th>
-	</tr>
-	<?php
-	foreach($everyone as $person)
-	{
-		extract($person);
-		echo "<tr><td>";
-		forms_hidden('user[]', $user_id);
-		echo "<span title=\"$class_nick Class\">$name</td><td>";
-		forms_checkbox("credit[$user_id]", '1', $credit);
-		echo "</td><td>";
-		forms_checkbox("dues[$user_id]", '1', $dues);
-		echo "</td><td>";
-		forms_text(30, "details[$user_id]", $details);
-		echo "</td></tr>";
-	}
-	?>
-	<tr><td colspan="3"><?php forms_submit('Update', 'Update') ?></td></tr>
-</table>
+<div class="name-list-container">
+	<table id="name-list" class="show-table">
+		<tr>
+			<th>Name</th>
+			<th>Credit?</th>
+			<th>Dues?</th>
+			<th>Details</th>
+		</tr>
+		<?php
+		foreach($everyone as $person)
+		{
+			extract($person);
+			echo "<tr><td>";
+			forms_hidden('user[]', $user_id);
+			echo "<span title=\"$class_nick Class\">$name</td><td>";
+			forms_checkbox("credit[$user_id]", '1', $credit);
+			echo "</td><td>";
+			forms_checkbox("dues[$user_id]", '1', $dues);
+			echo "</td><td>";
+			forms_text(30, "details[$user_id]", $details);
+			echo "</td></tr>";
+		}
+		?>
+		<tr><td colspan="3"><?php forms_submit('Update', 'Update') ?></td></tr>
+	</table>
+</div>
 </form>
 
 <!-- this script automatically adds data-th attributes to all <td> -->
